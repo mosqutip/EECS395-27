1903457b9e86c1b4caa8cdd05373edd119d5c366
Maciek Swiech <swiechfedora@gmail.com>
2013-04-30 23:12:04 -0500

    file for exploring data and control dependencies

---
+++ data-ctrl.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main()
+{
+    int x = 0;
+    int y = 1;
+    int z = 2;
+
+    if (x == 0) {
+        printf("%d\n", y);
+    } else {
+        printf("%d\n", z);
+    }
+
+    int n = z;
+
+    return n;
+}
